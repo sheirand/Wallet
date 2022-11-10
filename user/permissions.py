3d4689e8bca5b7badaf4e1487bb45fbbbd82a433
sheirand <eugene.osakovich@gmail.com>
2022-11-10 12:30:03 +0300

    [USER ENDPOINT] add: - custom permission is added (will be used to allow owner or admin to modify and delete user profile)

---
+++ user/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+
+
+class IsOwnerOrAdmin(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_staff:
+            return True
+        return obj.id == request.user.id
