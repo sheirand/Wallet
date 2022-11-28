8ef6c21203a368dcbb742c76b8735447b7844cc1
sheirand <eugene.osakovich@gmail.com>
2022-11-28 18:57:20 +0300

    [TEST WALLET] add&fix: - fixtures for client populated with transactions added - tests for transaction endpoint is added - permissions fixed

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
+import json
 import pytest
+
 from rest_framework.test import APIClient
 
 from user.models import User
@@ -60,3 +62,69 @@ def superuser_token(client, superuser):
     token = response.data["token"]
 
     return token
+
+
+@pytest.fixture
+def transaction():
+
+    transaction = {
+      "amount": "20.00",
+      "category": "Машина",
+      "organization": "Автосервис",
+      "description": "Замена масла двигателя",
+      "income": False
+    }
+
+    return transaction
+
+
+@pytest.fixture
+def another_transaction():
+    transaction = {
+        "amount": "50.00",
+        "category": "Зарплата",
+        "organization": "Завод",
+        "description": "Еще немного денег, чтобы дожить до следующей зарплаты",
+        "income": True
+    }
+
+    return transaction
+
+
+@pytest.fixture
+def client_with_transactions(client, user_token, transaction, another_transaction):
+
+    client.post('/api/v1/transaction/', transaction,
+                HTTP_AUTHORIZATION=f"{user_token}", format="json")
+
+    client.post('/api/v1/transaction/', another_transaction,
+                HTTP_AUTHORIZATION=f"{user_token}", format="json")
+
+    client.post('/api/v1/transaction/', transaction,
+                HTTP_AUTHORIZATION=f"{superuser_token}", format="json")
+
+    return client
+
+
+@pytest.fixture
+def transaction_id(client_with_transactions, user_token):
+
+    response = client_with_transactions.get('/api/v1/transaction/',
+                                            HTTP_AUTHORIZATION=f"{user_token}",
+                                            format='json')
+
+    data = json.loads(json.dumps(response.data))
+
+    return data["results"][0]["id"]
+
+
+@pytest.fixture
+def superuser_transaction_id(client_with_transactions, superuser_token):
+
+    response = client_with_transactions.get('/api/v1/transaction/',
+                                            HTTP_AUTHORIZATION=f"{superuser_token}",
+                                            format='json')
+
+    data = json.loads(json.dumps(response.data))
+
+    return data["results"][0]["id"]
